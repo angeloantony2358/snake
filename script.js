daa9df1f361eeeefbd55b9a5b240287240741468
Angelo Antony <83206032+angeloantony2358@users.noreply.github.com>
2022-02-27 23:06:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function drawSnakePart(snakePart) {
     ctx.fillStyle = 'white';
     ctx.strokestyle = 'white';
     ctx.fillRect(snakePart.x, snakePart.y, 20, 20);
-    ctx.strokeStyle(snakePart.x - 10, snakePart.y - 10, 10, 10);
+    ctx.stroke(snakePart.x - 10, snakePart.y - 10, 10, 10);
 }
 
 
